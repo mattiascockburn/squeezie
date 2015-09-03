bca1e701a081b680ade8c539399c003b17d6b459
Mattias Giese <giese@b1-systems.de>
2015-09-03 20:43:56 +0200

    really hacky scripts to build a void-linux based raspberry pi image

---
+++ rbpi-void/build.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+set -u
+set -e
+set -o pipefail
+cat <<EOF
+This script makes a few assumptions:
+
+  - you have enabled binfmt support for arm
+	- you have installed qemu-user-static
+	- the script needs kpartx in order to split the loopfs
+	- it uses sfdisk to partition the image file
+
+I am on Arch and just use the binaries provided by debian:
+https://packages.debian.org/sid/qemu-user-static
+
+This script is based on instructions found at
+https://github.com/voidlinux/documentation/wiki/Raspberry-Pi#rootfs-install
+EOF
+
+set +e
+error=0
+for bin in mkfs.vfat mkfs.ext4 tar parted dd kpartx losetup;do
+  if ! which $bin; then
+    echo $bin not found in PATH. Please make this binary available
+    error=1
+  fi
+done
+
+[ error -eq 1 ] && {
+  echo Essential binaries not found. Exiting.
+  exit 1
+}
+set -e
+
+# The rootfs to download and use
+VOIDURL=http://repo.voidlinux.eu/live/current
+VOIDIMAGE=void-rpi-rootfs-20150713.tar.xz
+
+SQUEEZEURL='http://squeezelite-downloads.googlecode.com/git'
+SQUEEZE='squeezelite-armv6hf'
+
+# Qemu Static stuff
+QEMU=/usr/local/bin/qemu-arm-static
+# squeezie specific kernel commandline for the resulting image
+KCMDLINE='wireless=0 squeezie-name=testing squeezie-master=http://squeeze.foo.bar:5555'
+
+# temp files like image download and squeezelite binary
+BUILD=$(pwd)/build
+
+BUILDDIR=$(mktemp -d /tmp/squeezie-build-XXX)
+
+
+IMAGENAME="squeezie-raspberry.img"
+
+IMAGESIZE='800M'
+
+echo Creating sparse image $IMAGENAME in $BUILDDIR
+mkdir -pv ${BUILDDIR}/{out,mnt}
+IPATH="${BUILDDIR}/out/${IMAGENAME}"
+MNT="${BUILDDIR}/mnt"
+dd if=/dev/zero of=${IPATH} bs=1 count=0 seek=$IMAGESIZE
+
+#parted /dev/mmcblk0 <- change this to match your SD card
+#
+## Create the FAT partition of 256MB and make it bootable
+#(parted) mktable msdos
+#(parted) mkpart primary fat32 2048s 256MB
+#(parted) toggle 1 boot
+#
+## Create the rootfs partition until end of device
+#(parted) mkpart primary ext4 256MB -1
+#(parted) quit
+
+echo Associating loop file with image
+LOOP=$(sudo losetup -f --show "$IPATH")
+LNAME=$(echo $LOOP | sed -e 's|^/dev/||g')
+
+ped() {
+	sudo parted -s -a optimal $LOOP -- $@
+}
+
+echo Downloading Void Linux image to ${BUILD}/${VOIDIMAGE}
+wget -c "${VOIDURL}/${VOIDIMAGE}" -O ${BUILD}/${VOIDIMAGE}
+
+echo Downloading squeezelite static
+wget -c "${SQUEEZEURL}/${SQUEEZE}" -O "${BUILD}/${SQUEEZE}"
+
+echo Creating partition table
+ped 'mktable msdos'
+ped 'mkpart primary fat32 2048s 256MB'
+ped 'toggle 1 boot'
+ped 'mkpart primary ext4 256MB -1'
+
+sudo kpartx -avs ${LOOP}
+
+echo Creating filesystems
+
+ROOTPART=/dev/mapper/${LNAME}p2
+BOOTPART=/dev/mapper/${LNAME}p1
+sudo mkfs.vfat -F32 $BOOTPART
+sudo mkfs.ext4 -O ^has_journal $ROOTPART
+
+echo Mounting image ar $MNT
+sudo mount "$ROOTPART" "${MNT}"
+sudo mkdir "${MNT}/boot"
+sudo mount "$BOOTPART" "${MNT}/boot"
+
+echo Extracting ${VOIDIMAGE}
+sudo tar xfJp ${BUILD}/${VOIDIMAGE} -C "${MNT}"
+sudo sync
+
+echo Setting up chroot
+
+# now actually do some work
+sudo cp $QEMU ${MNT}/usr/local/bin
+for a in sys dev proc;do
+	sudo mount --bind /$a ${MNT}/$a
+done
+sudo cp /etc/resolv.conf ${MNT}/etc/
+chmod +x ${BUILD}/${SQUEEZE}
+sudo cp ${BUILD}/${SQUEEZE} ${MNT}/usr/local/bin/squeezelite
+set +e
+sudo cp image-bootstrap.sh ${MNT}/
+sudo cp rc.conf ${MNT}/etc/
+#sudo chroot ${MNT} /bin/bash
+sudo chroot ${MNT} /image-bootstrap.sh
+
+sudo bash -c "echo > ${MNT}/etc/resolv.conf"
+echo Unmounting image
+sudo umount -R ${MNT}
+sudo kpartx -dvs ${LOOP}
+sudo losetup -d ${LOOP}
+
+mv ${IPATH} .
+#xz -T 0 -z -c ${IPATH} > ${IMAGENAME}.xz
+
+rm -rf ${BUILDDIR}
+
---
+++ rbpi-void/image-bootstrap.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -u
+# This script will be executed inside the chroot
+
+echo squeezie > /etc/hostname
+export XBPS_TARGET_ARCH=armv6l
+xbps-install -Suy
+xbps-install -Sy openssh python3 alsa-utils rpi-kernel rpi-firmware faad2 libmad python3.4-Flask python3.4-pip python3.4-requests
+
+useradd -m -G audio squeezie
+
+cat <<EOF >>/etc/fstab
+/dev/mmcblk0p1 /boot vfat defaults 0 0
+EOF
+
+pushd /etc/runit/runsvdir/default/
+rm agetty-tty{3,4,5,6} sshd
+for sv in alsa squeezelite;do
+  ln -s /etc/sv/$sv .
+done
+popd
+
+# Add system user for dbus service, wondering why this is not done automatically
+#useradd -r -U dbus
+
+rm -rf /usr/share/man /var/cache/xbps/* /usr/share/doc/*
+
+>/etc/resolv.conf
+
+mkdir /etc/sv/squeezelite
+
+cat <<EOF >/etc/squeezelite.cfg
+SERVER=squeeze.foo.bar
+NAME=squeezie
+OUTPUT=default:CARD=U0x41e0x30d3
+LOGLEVEL=info
+EOF
+
+
+cat <<EOF >/etc/sv/squeezelite/run
+#!/bin/sh
+sv start alsa || exit 1
+sv start dhcpcd || exit 1
+>/var/log/squeeze.log
+. /etc/squeezelite.cfg
+exec /usr/local/bin/squeezelite -s \$SERVER -n \$NAME -o \$OUTPUT -d all=\$LOGLEVEL -f /var/log/squeeze.log
+EOF
+
