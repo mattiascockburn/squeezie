7e0f641f57d157babe16391a3ce361e7cfd0fab7
Mattias Giese <giese@b1-systems.de>
2015-09-06 13:46:48 +0200

    even more i have no idea what i am doing

--- app/master.py
+++ app/master.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hello World!"
+@app.route("/",methods=['GET'])
+def index():
+  if request.method == 'GET':
+    return render_template('index.html')
+  else:
+    abort(400)
+
+@app.route("/devices",methods=['GET'])
+def devices():
+  if request.method == 'GET':
+    return render_template('devices.html')
+  else:
+    abort(400)
+
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.debug = True
+  app.run(host='0.0.0.0')
